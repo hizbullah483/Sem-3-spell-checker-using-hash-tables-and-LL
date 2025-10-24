220efe099e2a270c8f92ddf4fc3c3bf22777bbb1
hizbullah483 <hizbullahrehman483@gmail.com>
2025-10-24 23:24:36 +0500

    made it so special characters dont cause false negatives

--- DSA Project/spellChecker.cpp
+++ DSA Project/spellChecker.cpp
@@ -9,16 +9,17 @@ spellChecker::spellChecker(string dictionarytextFile){
     dict.loadDictionarytextFile(dictionarytextFile);
 }
 
-string spellChecker::lowerWord(const string &word){
+string spellChecker::lowerAndCleanWord(const string &word){
     string updatedWord;
-    for(int i = 0; i < word.length(); i++)
-        updatedWord += tolower(word[i]);
+    for(int i = 0; i < word.length(); i++){
+        if(isalpha(word[i])) updatedWord += tolower(word[i]);
+    }
     
     return updatedWord;
 }
 
 bool spellChecker::checkWord(const string &word){
-    string updatedWord = lowerWord(word);
+    string updatedWord = lowerAndCleanWord(word);
     return dict.search(updatedWord); //if true, no recommendation for that word
 }
 
@@ -26,7 +27,7 @@ void spellChecker::checkSentence(const string &sentence){
     stringstream streamedSentence(sentence);
     string word;
     while(streamedSentence >> word){
-        string updatedWord = lowerWord(word);
+        string updatedWord = lowerAndCleanWord(word);
 
         if(checkWord(updatedWord)) continue;
 
