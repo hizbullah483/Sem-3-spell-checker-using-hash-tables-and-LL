4d783fb3e9955d23983579136785638250ccbe4a
hizbullah483 <hizbullahrehman483@gmail.com>
2025-11-08 22:02:35 +0500

    Add files via upload

---
+++ DSA Project/hashTable.h
@@ -0,0 +1,37 @@
+#ifndef hashTableHeader
+#define hashTableHeader
+
+#include<vector>
+#include<string>
+using namespace std;
+
+//watch inception to know how hash tables and vectors relate
+struct Node{
+    string word;
+    Node *next;
+
+    Node(string w){
+        word = w;
+        next = nullptr;
+    }
+};
+
+class hashTable{
+    vector<Node*> table; //each element is the start of the linked list (ptr yo head)
+    int tableSize;
+
+    public:
+    vector<string> allWords;
+    int hashFunction(const string &word);
+    hashTable(int size = 100007);
+    
+    void insert(const string &word);
+    bool search(const string &word);
+    void loadDictionarytextFile(const string &filename);
+    void displayStats();
+    string lowerWord(const string &word);
+    ~hashTable();
+
+};
+
+#endif
